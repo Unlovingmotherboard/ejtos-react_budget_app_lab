e7fd40b9012f338a1b598770536d3fbecba04a71
Jose Hernandez <lovingmobo@Joses-MacBook-Pro.local>
2023-06-21 23:25:36 -0500

    added multiple components to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
+import Remaining from './components/Remaining'
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 // Add code to import the other components here under
 
 
@@ -15,13 +17,14 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
 
-                        {/* Add Remaining component here under */}        
+                        {<div className='col-sm'> <Budget /> </div>}
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {<div className='col-sm'> <Remaining /> </div>}        
+
+                        {<div className='col-sm'> <ExpenseTotal /> </div>}        
                        
-                        {/* Add ExpenseList component here under */}         
+                        {<div className='col-sm'></div>} ----------------------------------//STOPPED HERE     
 
                         {/* Add ExpenseItem component here under */}        
 
