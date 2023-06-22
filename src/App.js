a8d0105c255a0afaa76744a54877f2bdd3266cb8
Jose Hernandez <lovingmobo@Joses-MacBook-Pro.local>
2023-06-22 12:53:45 -0500

    working app after adding logic to components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining'
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 // Add code to import the other components here under
 
 
@@ -17,18 +18,29 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-
                         {<div className='col-sm'> <Budget /> </div>}
 
                         {<div className='col-sm'> <Remaining /> </div>}        
 
                         {<div className='col-sm'> <ExpenseTotal /> </div>}        
                        
-                        {<div className='col-sm'></div>} ----------------------------------//STOPPED HERE     
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
+                        {<div className='col-sm'>
+                        <h3 className='mt-3'>Allocation</h3>
+                        <div className='row'>
+                            <div className='col-sm'>
+                            <ExpenseList />
+                            </div>
+                        </div>
+                        </div>}    
+                        
+                        {<div> 
+                        <h3 className='mt-3'>Change allocation</h3>
+                        <div className='row mt-3'>
+                            <div className='col-sm'>
+                                <AllocationForm />
+                            </div>
+                        </div>
+                        </div>}        
 
                 </div>
             </div>
